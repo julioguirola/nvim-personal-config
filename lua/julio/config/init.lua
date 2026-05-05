c399ab85c24496c5b7da73fb6d7e102845b2d677
julioguirola <julioguirola03@gmail.com>
2026-05-05 12:02:26 -0400

    navegacion mantiene el pwd hasta q en entra en un repo de git, ahi se mantiene el pwd

--- lua/julio/config/init.lua
+++ lua/julio/config/init.lua
@@ -34,7 +34,51 @@ vim.diagnostic.config({
 	update_in_insert = false,
 })
 
--- vim.g.netrw_keepdir = 0
+-- Netrw nunca toca el cwd, nosotros lo controlamos todo
+vim.g.netrw_keepdir = 1
+
+local function find_git_root(path)
+	local dir = vim.fn.resolve(path)
+	while true do
+		if vim.fn.isdirectory(dir .. "/.git") == 1 or vim.fn.filereadable(dir .. "/.git") == 1 then
+			return dir
+		end
+		local parent = vim.fn.fnamemodify(dir, ":h")
+		if parent == dir then
+			return nil
+		end
+		dir = parent
+	end
+end
+
+local function update_cwd()
+	if vim.bo.filetype ~= "netrw" then
+		return
+	end
+
+	-- b:netrw_curdir es el directorio que netrw está mostrando actualmente
+	local curdir = vim.b.netrw_curdir
+	if not curdir or vim.fn.isdirectory(curdir) == 0 then
+		return
+	end
+
+	local git_root = find_git_root(curdir)
+
+	if git_root then
+		-- Dentro de un repo: cwd = git root, sin importar en qué subdir estés
+		vim.cmd("cd " .. vim.fn.fnameescape(git_root))
+	else
+		-- Fuera de cualquier repo: cwd = el directorio que netrw está mostrando
+		vim.cmd("cd " .. vim.fn.fnameescape(curdir))
+	end
+end
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	callback = function()
+		-- vim.schedule para esperar a que netrw termine de setear b:netrw_curdir
+		vim.schedule(update_cwd)
+	end,
+})
 
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldmethod = "expr"
@@ -54,3 +98,9 @@ vim.keymap.set({ "n", "v" }, "y", '"+y')
 vim.keymap.set("n", "Y", '"+Y')
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.keymap.set({ "n", "v" }, "x", '"_x', { noremap = true })
+vim.keymap.set({ "n", "v" }, "X", '"_X', { noremap = true })
+vim.keymap.set({ "n", "v" }, "d", '"_d', { noremap = true })
+vim.keymap.set({ "n", "v" }, "D", '"_D', { noremap = true })
+vim.keymap.set("n", "dd", '"_dd', { noremap = true })
